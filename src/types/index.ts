0ffad46b646a4f7198c3cb03b410a13b7b71bbd0
Sachin Kharel <kharelsachin08@gmail.com>
2026-05-09 14:35:29 +0930

    initial setup of website

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export interface User { id?: string; }
+export interface Job { id?: string; }
+export interface Company { id?: string; }
+export interface Room { id?: string; }
+export interface Application { id?: string; }
